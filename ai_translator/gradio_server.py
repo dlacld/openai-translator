e0d74a4ad13dbe6d372091024d8fb3c1004c5ee9
dlacld <1301816722@qq.com>
2025-05-03 23:56:42 +0800

    V1.1 Update

--- ai_translator/gradio_server.py
+++ ai_translator/gradio_server.py
@@ -20,7 +20,7 @@ def launch_gradio():
 
     iface = gr.Interface(
         fn=translation,
-        title="OpenAI-Translator v2.0(PDF 电子书翻译工具2250)",
+        title="glm-4-Translator v2.0(PDF 电子书翻译工具2250)",
         inputs=[
             gr.File(label="上传PDF文件"),
             gr.Textbox(label="源语言（默认：英文）", placeholder="English", value="English"),
